21404a9bc3306c35dc2432d35611c44706f94b67
Nicholas Cronquist <nick@ncronquist.com>
2020-05-03 20:54:29 -0400

    WIP

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { app, gqlServer } from './app';
+
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get('port'), () => {
+  console.log(
+    '  🚀 Server ready at http://localhost:%d%s in %s mode',
+    app.get('port'),
+    gqlServer.graphqlPath,
+    app.get('env'),
+  );
+  console.log('  Press CTRL-C to stop\n');
+});
+
+export default server;
